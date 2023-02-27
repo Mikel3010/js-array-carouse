bbbd5ee57faa2a0ea9164ea0621ce2e9b75099ea
Mikel3010 <mikelgremi30@gmail.com>
2023-02-27 21:40:14 +0100

    milestone3

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@
 
 const imgs = ['01.webp','02.webp','03.webp','04.webp','05.webp'];
 let itemsContent = '';
+let active = 0;
 
 const slider = document.querySelector('.slider');
 
@@ -17,3 +18,24 @@ for(let i = 0; i < imgs.length; i++){
 
 slider.innerHTML += itemsContent;
 document.querySelector('.item').classList.add('show');
+
+const prev = document.querySelector('.prev')
+const next = document.querySelector('.next')
+const items = document.querySelectorAll('.item')
+
+prev.addEventListener('click', function(){
+    if (active > 0){
+        items[active].classList.remove('show')
+        active--; 
+        items[active].classList.add('show')
+    }
+
+})
+
+next.addEventListener('click', function(){
+    if (active < items.length -1){
+        items[active].classList.remove('show')
+        active++; 
+        items[active].classList.add('show')
+    }
+})
