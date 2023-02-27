5351c4b931c42d5f02655539c8f47046f8d9ced0
Mikel3010 <mikelgremi30@gmail.com>
2023-02-27 21:35:37 +0100

    milestone 1 corretto

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
 'use strict'
+
+// <img class="item show" src="./img/01.webp" alt="">
+// <img class="item " src="./img/02.webp" alt="">
+// <img class="item " src="./img/03.webp" alt="">
+// <img class="item " src="./img/04.webp" alt="">
+// <img class="item " src="./img/05.webp" alt=""></img>
+
