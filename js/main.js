29d56d5c0779d086ff85a8a52d2ee76712e4efbd
Mikel3010 <mikelgremi30@gmail.com>
2023-02-27 21:39:13 +0100

    milestone 2

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
 'use strict'
 
-// <img class="item show" src="./img/01.webp" alt="">
-// <img class="item " src="./img/02.webp" alt="">
-// <img class="item " src="./img/03.webp" alt="">
-// <img class="item " src="./img/04.webp" alt="">
-// <img class="item " src="./img/05.webp" alt=""></img>
+/* <div class="item show"> <img src="./img/01.webp" alt=""></div>
+<div class="item"> <img src="./img/02.webp" alt=""></div>
+<div class="item"> <img src="./img/03.webp" alt=""></div>
+<div class="item"> <img src="./img/04.webp" alt=""></div>
+<div class="item"> <img src="./img/05.webp" alt=""></div> */
 
+const imgs = ['01.webp','02.webp','03.webp','04.webp','05.webp'];
+let itemsContent = '';
+
+const slider = document.querySelector('.slider');
+
+for(let i = 0; i < imgs.length; i++){
+    itemsContent += `<div class="item"><img src="./img/${imgs[i]}" alt=""></div>`;
+}
+
+slider.innerHTML += itemsContent;
+document.querySelector('.item').classList.add('show');
